04b6c7b0b26dcb99f43904283382fd4352e308e2
Niitrogen-o <84135773+Niitrogen-o@users.noreply.github.com>
2024-11-13 15:01:46 +0100

    Update loadNavbar.js

--- scripts/loadNavbar.js
+++ scripts/loadNavbar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "/navbar.html", true); // Ensure the path to `navbar.html` is correct here
+  xhr.open("GET", "navbar.html", true); // Ensure the path to `navbar.html` is correct here
 
   xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
